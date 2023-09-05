a6aaa77502da951b4665baeb54c45f04048aeb1e
wm <nazir.kuchmenov@gmail.com>
2023-09-05 06:45:28 +0300

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    id_emp int NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL,
+    title text,
+    birth_date date,
+    notes text
+);
+CREATE TABLE customers
+(
+    customer_id varchar(5) PRIMARY KEY NOT NULL,
+    company_name varchar(50) NOT NULL,
+    contact_name varchar(40) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY NOT NULL,
+    customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES employees(id_emp) NOT NULL,
+    order_date date NOT NULL,
+    ship_city varchar(30) NOT NULL
+)
+
+SELECT * FROM customers
+SELECT * FROM orders
+SELECT * FROM employees
